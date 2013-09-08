817b6fd907f9bf98681b411169ed634af5969279
xorphitus <xorphitus@gmail.com>
2013-09-09 01:22:01 +0900

    fix bug. setup.sh included .git and .gitignore

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ DIR=`dirname $0`
 cd $DIR
 for f in $(find . -maxdepth 1 -name '.?*'); do
     FILE=`echo $f | sed -e "s/\.\///"`
-    if [ $FILE == '.git' -a $FILE == '.gitignore' ]; then
+    if [ $FILE == '.git' -o $FILE == '.gitignore' ]; then
         continue
     fi
     ln -s $DIR/$FILE $HOME/$FILE
