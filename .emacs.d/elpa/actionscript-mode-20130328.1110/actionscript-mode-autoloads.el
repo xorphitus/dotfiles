9522f4d8ca3d3860b61e8bad2780082187c1d00b
xorphitus <xorphitus@gmail.com>
2013-09-11 00:04:16 +0900

    auto-install や 手動で入れたものを一部 elpa に移行. diff が非常に多量

---
+++ .emacs.d/elpa/actionscript-mode-20130328.1110/actionscript-mode-autoloads.el
@@ -0,0 +1,30 @@
+;;; actionscript-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (actionscript-mode) "actionscript-mode" "actionscript-mode.el"
+;;;;;;  (21039 13171 928928 363000))
+;;; Generated autoloads from actionscript-mode.el
+
+(autoload 'actionscript-mode "actionscript-mode" "\
+Major mode for editing Actionscript files.
+\\{actionscript-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("actionscript-mode-pkg.el") (21039 13171
+;;;;;;  942577 554000))
+
+;;;***
+
+(provide 'actionscript-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; actionscript-mode-autoloads.el ends here
---
+++ .emacs.d/elpa/shell-pop-20130908.1647/shell-pop-pkg.el
@@ -0,0 +1,1 @@
+(define-package "shell-pop" "20130908.1647" "helps you to use shell easily on Emacs. Only one key action to work. [github]" (quote nil))
