fabb3d98860699ebd92778a5020cf0cb0d4906cc
xorphitus <xorphitus@gmail.com>
2013-09-13 23:58:17 +0900

    upgrade packages.

--- .emacs.d/elpa/shell-pop-20130908.1647/shell-pop-autoloads.el
+++
@@ -1,36 +0,0 @@
-;;; shell-pop-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (shell-pop shell-pop-universal-key) "shell-pop"
-;;;;;;  "shell-pop.el" (21039 12698 862271 877000))
-;;; Generated autoloads from shell-pop.el
-
-(defvar shell-pop-universal-key nil "\
-Key binding used to pop in and out of the shell.
-
-The input format is the same as that of `kbd'.")
-
-(custom-autoload 'shell-pop-universal-key "shell-pop" nil)
-
-(autoload 'shell-pop "shell-pop" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("shell-pop-pkg.el") (21039 12698 871438
-;;;;;;  751000))
-
-;;;***
-
-(provide 'shell-pop-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; shell-pop-autoloads.el ends here
---
+++ .emacs.d/elpa/shell-pop-20130911.1953/shell-pop-pkg.el
@@ -0,0 +1,1 @@
+(define-package "shell-pop" "20130911.1953" "helps you to use shell easily on Emacs. Only one key action to work. [github]" (quote nil))
