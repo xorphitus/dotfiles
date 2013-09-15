d0dbcded243ab64c19fc2e8c7ae928c41309de0c
xorphitus <xorphitus@gmail.com>
2013-09-15 22:15:15 +0900

    現在使っていないorg-toodledoがエラーを吐いていたので削除

--- .emacs.d/elpa/org-toodledo-20130902.1042/org-toodledo-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "org-toodledo" "20130902.1042" "Toodledo integration for Emacs Org mode [github]" 'nil)
-
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -305,21 +305,6 @@
 (require 'edbi)
 (autoload 'edbi:open-db-viewer "edbi")
 
-;;; Todo
-
-;; org-toodledo
-(require 'org-toodledo)
-
-;; Useful key bindings for org-mode
-(add-hook 'org-mode-hook
-          (lambda ()
-            (local-unset-key "\C-o")
-            (local-set-key "\C-od" 'org-toodledo-mark-task-deleted)
-            (local-set-key "\C-os" 'org-toodledo-sync)))
-(add-hook 'org-agenda-mode-hook
-          (lambda ()
-            (local-unset-key "\C-o")
-            (local-set-key "\C-od" 'org-toodledo-agenda-mark-task-deleted)))
 
 ;;;
 ;;; JavaScript
