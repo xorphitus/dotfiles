1b909d5afcc387b9ea8cb16b930bb06186625b43
xorphitus <xorphitus@gmail.com>
2013-09-15 23:00:42 +0900

    古いパッケージを削除

--- .emacs.d/elpa/findr-20120124.849/findr-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; findr-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (20685 51252
-;;;;;;  754193))
-
-;;;***
-
-(provide 'findr-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; findr-autoloads.el ends here
