e0752d2734edfdeb95fdc25cb5ad5b050e8c15e3
xorphitus <xorphitus@gmail.com>
2013-09-23 12:12:19 +0900

    php周りの設定

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -469,6 +469,27 @@
 (add-hook 'sgml-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook 'emmet-mode)
 
+;;; PHP
+(autoload 'php-mode "php-mode")
+(add-to-list 'auto-mode-alist '("\\.php" . php-mode))
+(setq php-mode-force-pear t)
+
+(add-hook 'php-mode-hook 'flycheck-mode)
+
+;;; MMM
+(require 'mmm-mode)
+(setq mmm-global-mode 'maybe)
+(set-face-background 'mmm-default-submode-face nil)
+;; for php-mode in html-helper-mode
+(mmm-add-mode-ext-class nil "\\.php?\\'" 'html-php)
+(mmm-add-classes
+ '((html-php
+    :submode php-mode
+    :front "<\\?\\(php\\)?"
+    :back "\\?>")))
+(add-to-list 'auto-mode-alist '("\\.php?\\'" . html-mode))
+
+
 ;;; LESS
 (require 'less-css-mode)
 (add-to-list 'auto-mode-alist '("\\.less" . less-css-mode))
