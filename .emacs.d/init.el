658434155c5bec50e25e5854d056b9238bfbf565
xorphitus <xorphitus@gmail.com>
2013-09-29 23:38:53 +0900

    enable migemo.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -280,7 +280,15 @@
   (global-undo-tree-mode))
 
 ;; migemo
-;(require 'migemo)
+(require 'migemo)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+(migemo-init)
 
 ;; flymake
 (require 'flymake)
