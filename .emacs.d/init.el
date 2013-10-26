9d5ca7845a493270d60eff7d88002e80cb5e3560
xorphitus <xorphitus@gmail.com>
2013-10-26 19:25:16 +0900

    packageの初回インストール用関数を定義した

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,19 +5,73 @@
 
 ;; set load path
 (let ((default-directory "~/.emacs.d/elisp/"))
-   (setq load-path (cons default-directory load-path))
-    (normal-top-level-add-subdirs-to-load-path))
+  (setq load-path (cons default-directory load-path))
+  (normal-top-level-add-subdirs-to-load-path))
 (let ((default-directory "~/.emacs.d/elpa/"))
-    (setq load-path (cons default-directory load-path))
-    (normal-top-level-add-subdirs-to-load-path))
+  (setq load-path (cons default-directory load-path))
+  (normal-top-level-add-subdirs-to-load-path))
 
-;; mermelada - elisp repository
-;;  http://marmalade-repo.org/
-;;  'package-list-packages
-(require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+;; elpa
+(setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
+
+(defun xorphitus-setup ()
+  (interactive)
+  (progn (package-refresh-contents)
+         (mapc
+          (lambda (pkg)
+            (or (package-installed-p pkg)
+                (package-install pkg)))
+          '(;;; --- coding ---
+            actionscript-mode
+            clojure-mode
+            coffee-mode
+            color-theme
+            edbi
+            emmet-mode
+            haml-mode
+            ipython
+            js2-mode
+            ;;js2-refactor
+            less-css-mode
+            markdown-mode
+            mmm-mode
+            nrepl
+            paredit
+            php-mode
+            open-junk-file
+            rinari
+            ruby-block
+            ruby-compilation
+            sass-mode
+            scala-mode
+            scss-mode
+            ;;; --- others ---
+            auto-complete
+            auto-install
+            flycheck
+            flymake-cursor
+            ;;all
+            ;;all-ext
+            ;;fuzzy
+            helm
+            helm-ag
+            init-loader
+            ;;multiple-cursors
+            ;;redo+
+            ;;tabbar
+            magit
+            migemo
+            popup
+            python-mode
+            quickrun
+            shell-pop
+            twittering-mode
+            undo-tree
+            wgrep
+            yasnippet))))
+
 
 ;; init loader
 (require 'init-loader)
