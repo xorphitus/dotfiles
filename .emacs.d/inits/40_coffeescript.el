ef9ba422ece037f7719558fdfcbf69333b421a57
xorphitus <xorphitus@gmail.com>
2014-01-09 10:27:00 +0900

    coffeescript-modeでインデントがおかしい問題対応

--- .emacs.d/inits/40_coffeescript.el
+++ .emacs.d/inits/40_coffeescript.el
@@ -6,9 +6,7 @@
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-(add-hook 'coffee-mode-hook
-    '(lambda()
-        (set (make-local-variable 'tab-width) 2)))
+(custom-set-variables '(coffee-tab-width 2))
 
 ;; flycheck
 ;;  npm: coffeelint
