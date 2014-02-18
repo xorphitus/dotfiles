24add5e0bdf60f0204acfcfe01eb6aa35b18f7a6
xorphitus <xorphitus@gmail.com>
2014-02-19 01:08:35 +0900

    環境固有過ぎるものを追いやった

---
+++ adhoc/arch-desktop/xmonad.hs
@@ -0,0 +1,21 @@
+import System.IO
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.SetWMName
+
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , terminal    = "terminator"
+        , modMask     = mod4Mask
+        , borderWidth = 1
+        , startupHook = setWMName "LG3D"
+        }
