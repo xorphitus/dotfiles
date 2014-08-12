232e3e03ae48acf8a86babfa05b01949519578f2
xorphitus <xorphitus@gmail.com>
2014-08-13 00:05:59 +0900

    ace-jump-modeの導入

--- .emacs.d/inits/10_utils.el
+++ .emacs.d/inits/10_utils.el
@@ -119,3 +119,12 @@
 
 ;; anzu
 (global-anzu-mode +1)
+
+;; ace-jump-mode
+(autoload
+  'ace-jump-mode
+  "ace-jump-mode"
+  "Emacs quick move minor mode"
+  t)
+
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
