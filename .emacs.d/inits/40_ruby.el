b42019d8fd5cf12f2b24268d6051b7b471aabc71
xorphitus <xorphitus@gmail.com>
2014-08-13 00:15:59 +0900

    Rubyとみなすファイルを追加

--- .emacs.d/inits/40_ruby.el
+++ .emacs.d/inits/40_ruby.el
@@ -10,6 +10,8 @@
 
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake" . ruby-mode))
 
 ;; disable magic comment
