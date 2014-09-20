65c21661ec39e878a2d9ff3e4345a7a686c0b8b7
xorphitus <xorphitus@gmail.com>
2014-09-20 20:51:52 +0900

    railbow delimiterの色を濃くした

--- .emacs.d/inits/20_display.el
+++ .emacs.d/inits/20_display.el
@@ -115,6 +115,15 @@
 (require 'rainbow-delimiters)
 (global-rainbow-delimiters-mode t)
 
+(require 'cl-lib)
+(require 'color)
+(cl-loop
+ for index from 1 to rainbow-delimiters-max-face-count
+ do
+ (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+   (cl-callf color-saturate-name (face-foreground face) 90)))
+
+
 ;; golden-ratio
 ;; resizing automatically the windows you are working on
 ;; to the size specified in the "Golden Ratio"
