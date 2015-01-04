2fa3cdcf811a38ee57961f63aef8561515205fef
xorphitus <xorphitus@gmail.com>
2015-01-04 22:50:17 +0900

    helm-migemoの追加

--- .emacs.d/inits/15_helm.el
+++ .emacs.d/inits/15_helm.el
@@ -35,6 +35,22 @@
   '(progn
      (define-key helm-map (kbd "C-h") 'delete-backward-char)))
 
+;; helm-migemo
+(require 'helm-migemo)
+;; http://rubikitch.com/2014/12/19/helm-migemo/
+(eval-after-load "helm-migemo"
+  '(defun helm-compile-source--candidates-in-buffer (source)
+     (helm-aif (assoc 'candidates-in-buffer source)
+         (append source
+                 `((candidates
+                    . ,(or (cdr it)
+                           (lambda ()
+                             ;; Do not use `source' because other plugins
+                             ;; (such as helm-migemo) may change it
+                             (helm-candidates-in-buffer (helm-get-current-source)))))
+                   (volatile) (match identity)))
+       source)))
+
 ;; helm-ag
 ;; http://d.hatena.ne.jp/syohex/20130302/1362182193
 (lazyload helm-ag)
