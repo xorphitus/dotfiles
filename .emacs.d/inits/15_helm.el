3e29f5507e5f313ddd57c25c1d63b21191f09944
xorphitus <xorphitus@gmail.com>
2015-09-24 22:57:21 +0900

    helm-swoop が効かない問題があったので応急処置

--- .emacs.d/inits/15_helm.el
+++ .emacs.d/inits/15_helm.el
@@ -31,19 +31,20 @@
 
 ;; helm-migemo
 (use-package helm-migemo
-  :config
-  ;; http://rubikitch.com/2014/12/19/helm-migemo/
-  (defun helm-compile-source--candidates-in-buffer (source)
-    (helm-aif (assoc 'candidates-in-buffer source)
-        (append source
-                `((candidates
-                   . ,(or (cdr it)
-                          (lambda ()
-                            ;; Do not use `source' because other plugins
-                            ;; (such as helm-migemo) may change it
-                            (helm-candidates-in-buffer (helm-get-current-source)))))
-                  (volatile) (match identity)))
-      source)))
+  ;; :config
+  ;; ;; http://rubikitch.com/2014/12/19/helm-migemo/
+  ;; (defun helm-compile-source--candidates-in-buffer (source)
+  ;;   (helm-aif (assoc 'candidates-in-buffer source)
+  ;;       (append source
+  ;;               `((candidates
+  ;;                  . ,(or (cdr it)
+  ;;                         (lambda ()
+  ;;                           ;; Do not use `source' because other plugins
+  ;;                           ;; (such as helm-migemo) may change it
+  ;;                           (helm-candidates-in-buffer (helm-get-current-source)))))
+  ;;                 (volatile) (match identity)))
+  ;;     source))
+  )
 
 ;; helm-ag
 (use-package helm-ag
