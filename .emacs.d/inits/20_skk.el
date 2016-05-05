5e5e7cfa73c1cbe6a3792feac7eb1ab00ba95da4
xorphitus <xorphitus@gmail.com>
2016-05-05 20:48:15 +0900

    skkの辞書を設定

--- .emacs.d/inits/20_skk.el
+++ .emacs.d/inits/20_skk.el
@@ -15,4 +15,10 @@
   (progn
     ;; disable skk-isearch for migemo
     (setq skk-isearch-mode-enable nil)
-    (setq skk-search-start-mode 'latin)))
+    (setq skk-search-start-mode 'latin)
+    (setq skk-large-jisyo "/usr/share/skk/SKK-JISYO.L")
+    (setq skk-extra-jisyo-file-list
+          (list '("/usr/share/skk/SKK-JISYO.geo"
+                  "/usr/share/skk/SKK-JISYO.jinmei"
+                  "/usr/share/skk/SKK-JISYO.propernoun"
+                  "/usr/share/skk/SKK-JISYO.station")))))
