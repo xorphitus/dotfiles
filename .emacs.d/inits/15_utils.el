c15de45354d9ee9a771f331964e5a65efb82f5ef
xorphitus <xorphitus@gmail.com>
2016-07-11 23:46:02 +0900

    company-modeを局所的に導入

--- .emacs.d/inits/15_utils.el
+++ .emacs.d/inits/15_utils.el
@@ -25,6 +25,24 @@
     ;; select completion target by using C-n/C-p
     (setq ac-use-menu-map t)))
 
+;; comapny
+(use-package company
+  :config
+  (global-company-mode)
+  (setq company-idle-delay 0.1
+        company-minimum-prefix-length 2
+        company-selection-wrap-around t)
+
+  (bind-keys :map company-mode-map
+             ("C-i" . company-complete))
+  (bind-keys :map company-active-map
+             ("C-n" . company-select-next)
+             ("C-p" . company-select-previous)
+             ("C-s" . company-search-words-regexp))
+  (bind-keys :map company-search-map
+             ("C-n" . company-select-next)
+             ("C-p" . company-select-previous)))
+
 ;; undo-tree.el
 (use-package undo-tree
   :config
--- .emacs.d/inits/40_clojure.el
+++ .emacs.d/inits/40_clojure.el
@@ -21,19 +21,6 @@
 ;; The REPL buffer name  will look like cider project-name:port
 (setq nrepl-buffer-name-show-port t)
 
-;;; ac-cider
-(use-package ac-cider
-  :commands ac-cider
-  :init
-  (progn
-    (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-    (add-hook 'cider-mode-hook 'ac-cider-setup)
-    (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
-    (eval-after-load "auto-complete"
-      '(progn
-         (add-to-list 'ac-modes 'cider-mode)
-         (add-to-list 'ac-modes 'cider-repl-mode)))))
-
 ;;; compojure indentation
 (add-hook 'clojure-mode-hook
           (lambda()
