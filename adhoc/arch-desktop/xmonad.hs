ce5763f1e2b0d89c49ee6663d60605a2d10fa875
xorphitus <xorphitus@gmail.com>
2017-10-28 18:32:21 +0900

    xmobarが何故か隠れてしまうようになった問題への対処

--- adhoc/arch-desktop/xmonad.hs
+++ adhoc/arch-desktop/xmonad.hs
@@ -6,9 +6,11 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.SetWMName
 import XMonad.Layout
+import XMonad.Layout.Gaps
 import XMonad.Util.EZConfig -- additionalKeys
 
-myLayout = Full ||| Tall 1 (3/100) (1/2)
+
+myLayout = gaps[(U, 24), (R, 0)] $ Full ||| Tall 1 (3/100) (1/2)
 
 main = do
     xmproc <- spawnPipe "xmobar"
@@ -24,7 +26,7 @@ main = do
         , borderWidth = 1
         , startupHook = setWMName "LG3D"
         }
-         `additionalKeys`
+        `additionalKeys`
         [ ((0, 0x1008ff13), spawn "amixer sset Master 5%+ && notify-send -t 3 '♪▲' \"$(amixer sget Master | grep '%' | sed 's/^ *//g')\"")
         , ((0, 0x1008ff11), spawn "amixer sset Master 5%- && notify-send -t 3 '♪▼' \"$(amixer sget Master | grep '%' | sed 's/^ *//g')\"")
         , ((0, 0x1008ff12), spawn "amixer sset Master toggle && notify-send -t 3 '♪' \"$(amixer sget Master | grep '%' | sed 's/^ *//g')\"")
