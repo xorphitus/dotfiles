980569593cd7431d97f21bd47a677b7f6f954c68
xorphitus <xorphitus@gmail.com>
2017-11-08 13:57:08 +0900

    macでのフォントサイズ調整

--- .emacs.d/inits/cocoa-emacs-base.el
+++ .emacs.d/inits/cocoa-emacs-base.el
@@ -8,5 +8,5 @@
 
 ;;; Code:
 
-(set-face-attribute 'default nil :family "Ricty" :height 120)
+(set-face-attribute 'default nil :family "Ricty" :height 130)
 (set-fontset-font "fontset-default" 'japanese-jisx0208 '("Ricty"))
