74ad2a446350f30184af45153683cd0e1c70b561
xorphitus <xorphitus@gmail.com>
2017-11-10 13:02:44 +0900

    leiningenの設定を他から引っ越し

---
+++ .lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:dependencies [[org.clojure/tools.nrepl "0.2.13"]
+                       [slamhound "RELEASE"]
+                       [org.clojure/tools.trace "RELEASE"]
+                       [criterium "0.4.4"]]
+        :plugins [[lein-kibit "RELEASE"]
+                  [jonase/eastwood "RELEASE"]
+                  [refactor-nrepl "2.4.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]]}}
