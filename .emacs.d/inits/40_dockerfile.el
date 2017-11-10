97be8a44a9c0102f9ec6de82ca0678070789bdbb
xorphitus <xorphitus@gmail.com>
2017-11-10 15:23:06 +0900

    Dockerfile用の設定 (Spotifyが作ってくれてるっていうね)

---
+++ .emacs.d/inits/40_dockerfile.el
@@ -0,0 +1,12 @@
+;;; dockerfile.el --- Dockerfile settings
+
+;; Author: xorphitus <xorphitus@gmail.com>
+
+;;; Commentary:
+;;
+;; This file has Dockerfile settings.
+
+;;; Code:
+
+(use-package dockerfile-mode
+  :mode (("Dockerfile" . dockerfile-mode)))
