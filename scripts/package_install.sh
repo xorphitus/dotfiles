cd0a82193e735015237a1eb7329277fd3835f17f
xorphitus <xorphitus@gmail.com>
2017-11-19 02:05:12 +0900

    shell scriptの整備

--- scripts/package_install.sh
+++ scripts/package_install.sh
@@ -2,7 +2,14 @@
 
 #!/bin/bash
 
-. ./lib.sh
+if [ "$(uname)" == 'Darwin' ]; then
+    OS='Mac'
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    OS='Linux'
+else
+    echo "($(uname -a)) is not supported."
+    exit 1
+fi
 
 if [ $OS == 'Mac' ]; then
     if type brew > /dev/null 2>&1; then
@@ -19,7 +26,38 @@ else
         echo 'Error: You have to install yaourt!'
         exit 1
     fi
-fi
+f
+
+function pacman() {
+    pacman -Q $1 > /dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        pacman -S $1
+    fi
+}
+
+function aur() {
+    pacman -Q $1 > /dev/null 2>&1
+    if [ $? -eq 1 ]; then
+        yaourt -S $1
+    fi
+}
+
+function brew() {
+    local path="/usr/local/Cellar/$1"
+    if [ -d $path ]; then
+        echo $1 is alreadly installed
+    else
+        brew install $@
+    fi
+}
+
+function install() {
+    if [ $OS == 'Mac' ]; then
+        brew $@
+    else
+        pacman $@
+    fi
+}
 
 ### enable gcc and other cli tools
 if [ $OS == 'Mac' ]; then
