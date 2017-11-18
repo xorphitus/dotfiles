09c295dfbf4726eaa89197b8a7ca33823694464f
xorphitus <xorphitus@gmail.com>
2017-11-19 02:00:50 +0900

    TypeScript設定追加

---
+++ .emacs.d/inits/40_typescript.el
@@ -0,0 +1,26 @@
+;;; typescript.el --- TypeScript settings
+
+;; Author: xorphitus <xorphitus@gmail.com>
+
+;;; Commentary:
+;;
+;; This file has TypeScript settings.
+
+;;; Code:
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(use-package tide
+  :config
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+  ;; formats the buffer before saving
+  (add-hook 'before-save-hook 'tide-format-before-save)
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
