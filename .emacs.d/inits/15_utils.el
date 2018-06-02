176273abd7a80279aca2bd37d4ad9cc9bf5795e1
xorphitus <xorphitus@gmail.com>
2018-06-02 21:18:12 +0900

    emacs direnv itegration

--- .emacs.d/inits/15_utils.el
+++ .emacs.d/inits/15_utils.el
@@ -129,7 +129,27 @@
 
 ;; neotree
 (use-package neotree
-  :bind (([f8] . neotree-toggle)))
+  :bind (([f8] . neotree-toggle))
+  :config
+  (setq neo-smart-open t))
+
+;;(setq projectile-switch-project-action 'neotree-projectile-action)
+;;
+;;(defun neotree-project-dir ()
+;;  "Open NeoTree using the git root."
+;;  (interactive)
+;;  (let ((project-dir (projectile-project-root))
+;;        (file-name (buffer-file-name)))
+;;    (neotree-toggle)
+;;    (if project-dir
+;;        (if (neo-global--window-exists-p)
+;;            (progn
+;;              (neotree-dir project-dir)
+;;              (neotree-find file-name)))
+;;      (message "Could not find git project root."))))
+;;
+;;(global-set-key [f8] 'neotree-project-dir)
+
 
 ;; org-tree-slide
 (use-package org-tree-slide
@@ -194,3 +214,7 @@
 (use-package wrap-region
   :config
   (wrap-region-mode t))
+
+(use-package direnv
+ :config
+ (direnv-mode))
