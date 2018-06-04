3398f6dd4167c0b7f9ffad41931348d6eb391c23
xorphitus <xorphitus@gmail.com>
2018-06-04 20:27:13 +0900

    cider-nreplのバージョンをciderのものに合わせた

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -5,4 +5,4 @@
         :plugins [[lein-kibit "RELEASE"]
                   [jonase/eastwood "RELEASE"]
                   [refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]]}}
+                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]]}}
