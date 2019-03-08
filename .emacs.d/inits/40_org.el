81dcbb33fec1549d2727313d1743c51abc93ddba
xorphitus <xorphitus@gmail.com>
2019-03-09 01:09:21 +0900

    fix org-bullets symbols

--- .emacs.d/inits/40_org.el
+++ .emacs.d/inits/40_org.el
@@ -15,7 +15,9 @@
 ;; beautify org-mode list bullets
 (use-package org-bullets
   :config
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  :custom
+  (org-bullets-bullet-list '("🌕" "🌔" "🌓" "🌒" "🌑")))
 
 ;; pomodoro
 (use-package org-pomodoro
