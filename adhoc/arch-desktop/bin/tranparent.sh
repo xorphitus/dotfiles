e17ba6cbebcb213dc7eeeb13bdde3d78ebeadf63
xorphitus <xorphitus@gmail.com>
2019-10-04 11:28:04 +0900

    refactor transparency script

--- adhoc/arch-desktop/bin/tranparent.sh
+++ adhoc/arch-desktop/bin/tranparent.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-readonly target=$1
-readonly opcity=$2
-
 abort() {
   echo "error: you have to install package \`$1\`" >&2
-  exit 0
+  exit 1
 }
+
+set -eu
+: $1 $2
+
+readonly target=$1
+readonly opcity=$2
 
 type xwininfo    > /dev/null || abort 'xorg-xwininfo'
 type transset-df > /dev/null || abort 'transset-df'
@@ -17,3 +20,4 @@ echo '--------'
 echo $targets
 
 echo $targets | xargs -I {} transset-df $opcity -i {} > /dev/null
+
