a461a4b5e59340b0b8928bcc40e8c72169ea5076
xorphitus <xorphitus@gmail.com>
2019-10-10 15:52:00 +0900

    refactor

--- .emacs.d/inits/20_skk.el
+++ .emacs.d/inits/20_skk.el
@@ -15,27 +15,26 @@
   :bind (("C-o"     . skk-mode)
          ("C-x C-j" . skk-mode))
   :config
-  (progn
-    ;; enable AZIK
-    (setq skk-use-azik t)
-    ;; disable skk-isearch for migemo
-    (setq skk-isearch-mode-enable nil)
-    (setq skk-search-start-mode 'latin)
-    ;; candidates position
-    (setq skk-show-tooltip t)
-    ;; dynamic completion
-    ;;   てん -> てんさい, てんしょく、てんかん
-    (setq skk-dcomp-activate t)
-    (setq skk-dcomp-multiple-activate t)
-    (setq skk-dcomp-multiple-rows 10)
-    ;; dictionary
-    (setq skk-large-jisyo (concat my-skk-jisyo-root "/SKK-JISYO.L"))
-    (setq skk-extra-jisyo-file-list
-          (--map (concat my-skk-jisyo-root it)
-                 '("/SKK-JISYO.geo"
-                   "/SKK-JISYO.jinmei"
-                   "/SKK-JISYO.propernoun"
-                   "/SKK-JISYO.station")))))
+  ;; enable AZIK
+  (setq skk-use-azik t)
+  ;; disable skk-isearch for migemo
+  (setq skk-isearch-mode-enable nil)
+  (setq skk-search-start-mode 'latin)
+  ;; candidates position
+  (setq skk-show-tooltip t)
+  ;; dynamic completion
+  ;;   てん -> てんさい, てんしょく、てんかん
+  (setq skk-dcomp-activate t)
+  (setq skk-dcomp-multiple-activate t)
+  (setq skk-dcomp-multiple-rows 10)
+  ;; dictionary
+  (setq skk-large-jisyo (concat my-skk-jisyo-root "/SKK-JISYO.L"))
+  (setq skk-extra-jisyo-file-list
+        (--map (concat my-skk-jisyo-root it)
+               '("/SKK-JISYO.geo"
+                 "/SKK-JISYO.jinmei"
+                 "/SKK-JISYO.propernoun"
+                 "/SKK-JISYO.station"))))
 
 (use-package ddskk-posframe
   :config
