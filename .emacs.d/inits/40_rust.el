e39632aefbfe0af6a9c9f8b918d26057abdfbdac
xorphitus <xorphitus@gmail.com>
2019-10-10 10:53:24 +0900

    patch rust-mode

--- .emacs.d/inits/40_rust.el
+++ .emacs.d/inits/40_rust.el
@@ -23,3 +23,18 @@
   ;;()
   :config
   (setq rustic-rls-pkg 'eglot))
+
+
+;;;https://github.com/joaotavora/eglot/issues/98
+(defun my-project-try-cargo-toml (dir)
+  (when-let* ((output
+               (let ((default-directory dir))
+                 (shell-command-to-string "cargo metadata --no-deps --format-version 1")))
+              (js (ignore-errors (json-read-from-string output)))
+              (found (cdr (assq 'workspace_root js))))
+    (cons 'eglot-project found)))
+
+(cl-defmethod project-roots ((project (head eglot-project)))
+  (list (cdr project)))
+
+(add-hook 'project-find-functions 'my-project-try-cargo-toml nil nil)
