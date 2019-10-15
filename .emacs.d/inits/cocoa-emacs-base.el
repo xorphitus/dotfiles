ebcd4276a1143fcf22bdf838b13f31b7aa60eb0a
xorphitus <xorphitus@gmail.com>
2019-10-15 22:54:26 +0900

    hunspell dict setting for mac

--- .emacs.d/inits/cocoa-emacs-base.el
+++ .emacs.d/inits/cocoa-emacs-base.el
@@ -17,6 +17,18 @@
 
 (setq my-skk-jisyo-root "~/skk")
 
+;; Need to place hunspell dictionaries
+;; to the path which is included in the results of `hunspell -D`
+;;
+;; $ cd ~/Library/Spelling
+;; $ wget https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.aff
+;; $ wget https://cgit.freedesktop.org/libreoffice/dictionaries/plain/en/en_US.dic
+(setq ispell-hunspell-dict-paths-alist '(("en_US" "~/Library/Spelling/en_US.aff"))
+      ispell-dictionary "en_US")
+
+;; hogee is me
+;; this is mineee ok
+
 ;; beautify powerline
 ;; https://github.com/milkypostman/powerline/issues/54
 (setq ns-use-srgb-colorspace nil)
