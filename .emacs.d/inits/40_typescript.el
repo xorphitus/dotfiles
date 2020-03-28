adf771f665fe4653bb47bc0b157b0ecd8e1a48f6
xorphitus <xorphitus@gmail.com>
2020-03-28 15:27:30 +0900

    fix typescript setting

--- .emacs.d/inits/40_typescript.el
+++ .emacs.d/inits/40_typescript.el
@@ -18,9 +18,15 @@
   (company-mode +1))
 
 (use-package tide
+  :mode (("\\.ts" . tide-mode)
+         ("\\.tsx" . tide-mode))
   :config
   ;; aligns annotation to the right hand side
   (setq company-tooltip-align-annotations t)
   ;; formats the buffer before saving
   (add-hook 'before-save-hook 'tide-format-before-save)
   (add-hook 'typescript-mode-hook #'setup-tide-mode))
+
+(use-package typescript
+  :mode (("\\.ts" . typescript-mode)
+         ("\\.tsx" . typescript-mode)))
