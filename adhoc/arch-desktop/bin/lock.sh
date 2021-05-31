e4b90ed2eb2d2cef86d50e08902495d519de7cac
xorphitus <xorphitus@gmail.com>
2021-06-01 08:18:42 +0900

    fix i3lock setting to the latest

--- adhoc/arch-desktop/bin/lock.sh
+++ adhoc/arch-desktop/bin/lock.sh
@@ -10,29 +10,29 @@ V='#00bbbb55'  # verifying
 export LC_ALL=C
 
 i3lock \
---insidevercolor=$C   \
---ringvercolor=$V     \
+--insidever-color=$C   \
+--ringver-color=$V     \
 \
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
+--insidewrong-color=$C \
+--ringwrong-color=$W   \
 \
---insidecolor=$B      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
+--inside-color=$B      \
+--ring-color=$D        \
+--line-color=$B        \
+--separator-color=$D   \
 \
---verifcolor=$T        \
---wrongcolor=$T        \
---timecolor=$T        \
---datecolor=$T        \
---layoutcolor=$T      \
---keyhlcolor=$W       \
---bshlcolor=$W        \
+--verif-color=$T        \
+--wrong-color=$T        \
+--time-color=$T        \
+--date-color=$T        \
+--layout-color=$T      \
+--keyhl-color=$W       \
+--bshl-color=$W        \
 \
 --screen 1            \
 --blur 16              \
 --clock               \
 --indicator           \
---timestr="%H:%M:%S"  \
---datestr="%A, %m %Y" \
+--time-str="%H:%M:%S"  \
+--date-str="%A, %m %Y" \
 --keylayout 2
