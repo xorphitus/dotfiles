aae8aeb8fa7500a9226f7ba5bc955fe0b8dcc8de
xorphitus <xorphitus@gmail.com>
2024-04-05 11:03:51 +0900

    enhance spotify control

--- envs/arch/bin/spotify.sh
+++ envs/arch/bin/spotify.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 spotifyd_stat() {
-  meta=$(playerctl metadata -p spotifyd)
-  title=$(echo "$meta" | grep title | awk '{print $3}')
-  artist=$(echo "$meta" | grep artist | awk '{print $3}')
-  echo "${title} / ${artist}" | sed 's/  */ /g'
+  meta=$(playerctl metadata -f '{{status}} {{title}} / {{artist}}' -p spotifyd)
+  echo "$meta" | sed 's/  */ /g' | sed 's/Playing/♪/g' | sed 's/Paused/⏸/g'
+}
+
+spotifyd_play_pause() {
+  playerctl play-pause -p spotifyd
+}
+
+spotifyd_next() {
+  playerctl next -p spotifyd
+}
+
+spotifyd_previous() {
+  playerctl previous -p spotifyd
 }
 
 ncspot_stat() {
@@ -26,15 +36,54 @@ ncspot_stat() {
   fi
 }
 
-# Disabled spotifyd support: The status should be displayed in another machine
-# via Spotify Connect
-#
-# if pgrep -x spotifyd > /dev/null; then
-#   spotifyd_stat
-# elif pgrep -x ncspot > /dev/null; then
-#   ncspot_stat
-# fi
+ncspot_play_pause() {
+  "echo playpause | nc -W 1 -U ~/.cache/ncspot/ncspot.sock"
+}
 
-if pgrep -x ncspot > /dev/null; then
-  ncspot_stat
+ncspot_next() {
+  "echo next      | nc -W 1 -U ~/.cache/ncspot/ncspot.sock"
+}
+
+ncspot_previous() {
+  "echo previous  | nc -W 1 -U ~/.cache/ncspot/ncspot.sock"
+}
+
+command=$1
+
+if pgrep -x spotifyd > /dev/null; then
+  case "$command" in
+    stat)
+      spotifyd_stat
+      ;;
+    play_pause)
+      spotifyd_play_pause
+      ;;
+    next)
+      spotifyd_next
+      ;;
+    previous)
+      spotifyd_previous
+      ;;
+    *)
+      spotifyd_stat
+      ;;
+  esac
+elif pgrep -x ncspot > /dev/null; then
+   case "$1" in
+    stat)
+      ncspot_stat
+      ;;
+    play_pause)
+      ncspot_play_pause
+      ;;
+    next)
+      ncspot_next
+      ;;
+    previous)
+      ncspot_previous
+      ;;
+    *)
+      ncspot_stat
+      ;;
+  esac
 fi
